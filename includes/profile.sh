f643f4d5f40f35cfcea4e8fb3350aa5120eb75df
Roman Kamyk <rkj@google.com>
2024-09-26 17:05:12 -0700

    Add countdown function

--- includes/profile.sh
+++ includes/profile.sh
@@ -194,5 +194,14 @@ if [ ! -z "$SSH_AUTH_SOCK" -a "$SSH_AUTH_SOCK" != "$HOME/.ssh/agent_sock" ] ; th
     export SSH_AUTH_SOCK="$HOME/.ssh/agent_sock"
 fi
 
+countdown() {
+  start="$(( $(date '+%s') + $1))"
+  while [ $start -ge $(date +%s) ]; do
+    time="$(( $start - $(date +%s) ))"
+    printf '%s\r' "$(date -u -d "@$time" +%H:%M:%S)"
+    sleep 0.1
+  done
+}
+
 source <(cat ~/.profiles/*.sh)
 
