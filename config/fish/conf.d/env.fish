78e4e853189fa26368d98cc3dc257d3f73c24bb5
Roman Kamyk <git@kamyk.me>
2026-04-10 21:42:07 -0700

    Clean up public dotfiles bootstrap and licensing

--- config/fish/conf.d/env.fish
+++ config/fish/conf.d/env.fish
@@ -10,16 +10,22 @@ if test -z "$DOTFILES"
     end
 end
 
-# Source shared environment variables
-if test -n "$DOTFILES" -a -f "$DOTFILES/env"
-    for line in (cat "$DOTFILES/env" | grep "^export ")
-        set -l kv (string split -m 1 "=" (string sub -s 8 $line))
-        set -gx $kv[1] (string trim -c "'" -c '"' $kv[2])
+# Source shared environment variables from tracked and local overlays.
+function source_dotfiles_env --argument-names file_path
+    if test -f "$file_path"
+        for line in (cat "$file_path" | grep "^export ")
+            set -l kv (string split -m 1 "=" (string sub -s 8 $line))
+            set -gx $kv[1] (string trim -c "'" -c '"' $kv[2])
+        end
     end
+end
+
+if test -n "$DOTFILES"
+    source_dotfiles_env "$DOTFILES/env"
+    source_dotfiles_env "$DOTFILES/env.local"
 end
 
 # Paths
 test -n "$DOTFILES"; and fish_add_path "$DOTFILES/bin"
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
-test -d /google/bin/releases/editor-devtools; and fish_add_path /google/bin/releases/editor-devtools
