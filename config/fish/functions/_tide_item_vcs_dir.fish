8825b42235b390a53df702f1c54eeb291c988e86
Roman Kamyk <git@kamyk.me>
2026-04-17 21:36:54 -0700

    Fix vcs_dir background color and conflict detection

--- config/fish/functions/_tide_item_vcs_dir.fish
+++ config/fish/functions/_tide_item_vcs_dir.fish
@@ -1,15 +1,20 @@
 function _tide_item_vcs_dir
-    set -l home_regex (string escape --style=regex $HOME)
-
     set -l text (_tide_item_vcs_status_text)
     
     # Determine background color based on status symbols
-    if string match -q '*~*' "$text"
+    # Conflict marker is ' ~' followed by a number.
+    # Status markers are ' +', ' !', ' ?'
+    # Repo icons are at the start: , 🥋, ☿
+    
+    if string match -qr ' ~[0-9]' "$text"
         set -g tide_vcs_dir_bg_color $tide_git_bg_color_urgent
-    else if string match -r '[+!?]' "$text"
+    else if string match -qr ' [+!?]' "$text"
         set -g tide_vcs_dir_bg_color $tide_git_bg_color_unstable
+    else if string match -qr '^[🥋☿]' "$text"
+        set -g tide_vcs_dir_bg_color $tide_git_bg_color
+    else
+        set -g tide_vcs_dir_bg_color $tide_pwd_bg_color
     end
 
-    # Print the item with branch color (black usually) and the text
     _tide_print_item vcs_dir (set_color $tide_git_color_branch; echo -ns "$text")
 end
